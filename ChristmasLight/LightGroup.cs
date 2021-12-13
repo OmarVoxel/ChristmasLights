2f68e557334152f80dc35186e5237027cb197c3d
SharkiCS <Sharkismanagements@gmail.com>
2021-12-14 00:34:49 +0100

    refactor: added lightgroup class

---
+++ ChristmasLight/LightGroup.cs
@@ -0,0 +1,22 @@
+﻿namespace ChristmasLight
+{
+    public class LightGroup
+    {
+        private const int M = 1000;
+        private const int N = 1000;
+
+        private Bulb[,] _matrix = new Bulb[N,M];
+
+        public LightGroup()
+            => Initialize();
+
+        private void Initialize()
+        {
+            for (int x = 0; x < M; x++)
+                for (int y = 0; y < N; y++)
+                    _matrix[x, y] = new Bulb();
+        }
+
+        public Bulb At(int x, int y) => _matrix[x, y];
+    }
+}
