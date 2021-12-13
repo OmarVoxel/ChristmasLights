182e6d4968c6c0da360d38a8804250834f09327d
DF Lorenzo <three@hey.com>
2021-12-14 00:10:25 +0100

    test: BulbsAtPositionsAreOffByDefault

--- ChristmasLight.Tests/ChristmasTests.cs
+++ ChristmasLight.Tests/ChristmasTests.cs
@@ -32,5 +32,17 @@ namespace ChristmasLight.Tests
             bulb.TurnOff();
             bulb.Status().Should().Be(0);
         }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(999, 0)]
+        [InlineData(0, 999)]
+        [InlineData(999, 999)]
+        public void BulbsAtPositionsAreOffByDefault(int x, int y)
+        {
+            LightGroup lightGroup = new LightGroup();
+            lightGroup.At(x, y).Status().Should().Be(0);
+        }
+        
     }
 }
